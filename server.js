e47227cf061fb08b124ee5ae1e3fb0d75cfd9135
AHMADNURMALIK <ahmadnur.malik41@gmail.com>
2026-05-08 15:51:35 +0700

    fix mysql duplicate

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
-const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 
-const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
-
-db.connect((err) => {
-    if(err){
-        console.log("Database Error:", err);
-    } else {
-        console.log("Database Connected");
-    }
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
